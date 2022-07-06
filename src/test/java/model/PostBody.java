ba41a638cb8e7e8c8eac72bf456907aa3ac1a87e
ttg990325 <truonggiang25031999@gmail.com>
2022-07-07 06:40:49 +0700

    post method bs

---
+++ src/test/java/model/PostBody.java
@@ -0,0 +1,24 @@
+package model;
+
+public class PostBody {
+    private int userId;
+    private String id;
+    private String title;
+    private String body;
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
--- src/test/java/test/POSTMethod.java
+++ src/test/java/test/POSTMethod.java
@@ -1,7 +1,13 @@
 package test;
+import com.google.gson.Gson;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import model.PostBody;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
+
 
 import static io.restassured.RestAssured.given;
 
@@ -16,17 +22,33 @@ public class POSTMethod {
         request.header(new Header("Content-type", "application/json; charset=UTF-8"));
 
         //Form up request body
-        String postBody = "{\n" +
-                "  \"userId\": 1,\n" +
-                "  \"id\": 1,\n" +
-                "  \"title\": \"The req's' title\",\n" +
-                "  \"body\": \"The req's body\"\n" +
-                "}";
+//        String postBody = "{\n" +
+//                "  \"userId\": 1,\n" +
+//                "  \"id\": 1,\n" +
+//                "  \"title\": \"The req's' title\",\n" +
+//                "  \"body\": \"The req's body\"\n" +
+//                "}";
+        //Gson
+        Gson gson = new Gson();
+        PostBody postBody = new PostBody();
+        postBody.setUserId(1);
+        postBody.setId("1");
+        postBody.setTitle("The req's' title");
+        postBody.setBody("The req's body");
+
         // Send POST request
-        Response response = request.body(postBody).post("/posts");
+//        Response response = request.body(postBody).post("/posts");
+        Response response = request.body(gson.toJson(postBody)).post("/posts");
         response.prettyPrint();
 
-        System.out.println(response.statusCode());
+        //Verification
+        response.then().statusCode(equalTo(201));
+        response.then().statusLine(containsStringIgnoringCase("201 Created"));
+        response.then().body("userId", equalTo(1));
+        response.then().body("title", equalTo("The req's' title"));
+        response.then().body("body", equalTo("The req's body"));
+
+
     }
 
 }
